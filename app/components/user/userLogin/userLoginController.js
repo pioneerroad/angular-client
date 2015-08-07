014499836c27bd7f2e08c90392eceda7ea6e645e
Matthew Schwarze <matthew.schwarze@gmail.com>
2015-08-07 16:52:43 +1000

    user login
    can now get token data

--- app/components/user/userLogin/userLoginController.js
+++ app/components/user/userLogin/userLoginController.js
@@ -1,30 +1,23 @@
 (function () {
-
-    angular.module('pioneerRoad')
-            .controller('userLoginController', ['$scope', '$http', 'userLoginService', function ($scope, $http, userLoginService) {
+     var app = angular.module("pioneerRoad");
+    
+            app.controller('userLoginController', ['$scope', '$http', 'userLoginService', function ($scope, $http, userLoginService) {
                     $scope.messageBool = false;
-
 
                     $scope.closeError = function () {
                         $scope.messageBool = false;
                     };
 
                     $scope.authenticate = function () {
-                        userLoginService.Login($scope.username, $scope.password)
+                        userLoginService.Login($scope.email, $scope.password)
                                 .success(function (response) {
-                                    if (response.token) {
+                                    if (response) {
                                         // add to the location storage
                                         console.log("good");
                                     }
                                 })
                                 .error(function (error) {
                                     console.log(error);
-
-
-                                    // $timeout(function() {
-                                    //   $scope.messageBool = false;
-                                    // }, 2000);
-                                    // add a message saying unauthenticated
                                 });
                     };
                 }]);
