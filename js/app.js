4e14691d646f87383cf3f7088ca7fc3b9e90558b
Matt Clark <matt@pioneerroad.com.au>
2015-08-13 15:42:53 +1000

    Updated master

--- js/app.js
+++
@@ -1,6 +0,0 @@
-(function() {
-      var app = angular.module("pioneerRoad", ["ui.bootstrap.modal"]); //ui.bootstrap.modal is used for the interactions between angular and the modals
-      
-}());
-
-
