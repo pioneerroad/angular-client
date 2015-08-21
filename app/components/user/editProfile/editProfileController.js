d45ec75579c17f25a96e1396096f954221351f7c
Matthew Schwarze <matthew.schwarze@gmail.com>
2015-08-21 14:06:17 +1000

    change name, home town working
    to be implemented; check in, auto check in service.

--- app/components/user/editProfile/editProfileController.js
+++ app/components/user/editProfile/editProfileController.js
@@ -7,95 +7,117 @@
                 $location.path("/login");
                 console.log("i'm not logged in");
             }
-            
+
             $scope.updateNickName = function () {
                 console.log($scope.nickname);
                 editProfileService.changeNickName($scope.nickname)
-                    .success(function (response) {
-                        console.log(response);
-                        getCurrentProfile();
-                    })
-                    .error(function (error) {
-                        console.log(error);
-                    });
-            };
-            
-            $scope.getHomeTownSug = function(){
-              if(keyPressCount > 3){
-                  //send current $scope.hometown to /town/select/:input
-                  editProfileService.getHomeTownSuggestions($scope.hometown)
-                  .success(function (response) {
-                        
-                            $scope.homeTownSuggestions = response;
+                        .success(function (response) {
                             console.log(response);
-                       
-                    })
-                    .error(function (error) {
-                        console.log(error);
-                    });
+                            getCurrentProfile();
+                        })
+                        .error(function (error) {
+                            console.log(error);
+                        });
+            };
 
-              }else{
-                  keyPressCount++;
-              }
+            $scope.getHomeTownSug = function () {
+                if (keyPressCount > 2) {
+                    //send current $scope.hometown to /town/select/:input
+                    editProfileService.getHomeTownSuggestions($scope.hometown)
+                            .success(function (response) {
+                                $scope.homeTownSuggestions = response; 
+
+                            })
+                            .error(function (error) {
+                                console.log(error);
+                            });
+
+                } else {
+                    keyPressCount++;
+                }
             };
-            
-            $scope.chosenTown= function(town){
-              $scope.hometown = town.label;
-              $scope.chosenHomeTown.label = $scope.hometown;
-              $scope.chosenHomeTown.id = town.id;
-              
+
+            //called when user clicks a suggestion
+            $scope.chosenTown = function (town) {
+                $scope.hometown = town.label; //set the view hometown to the click town
+                $scope.getHomeTownSug(); // forces update of suggestions
+
             };
-            
-            $scope.updateHomeTown = function(){
-                keyPressCount = 0;
+
+            $scope.updateHomeTown = function () {
+
                 $scope.chosenHomeTown.label = $scope.hometown;
-                //call the suggest function to get correct information using the user inputted town, as if they did not use suggestions
-                //there will be no town id
-                //post/put information to users profile
-                //figure out town id -> town name stuff
+                $scope.getHomeTownSug(); //now there should only be one entry in $scope.homeTownSuggestions
+                editProfileService.setHomeTown($scope.homeTownSuggestions[0].id)
+                        .success(function (response) {
+                            console.log("profile: town updated");
+                            tearDown();
+                        })
+                        .error(function (error) {
+                            console.log(error);
+                            tearDown();
+                        });
+
+               
+                 //update the view 
+
+
             };
 
             var getCurrentProfile = function () { //get the users profile
-                
+                console.log("getting new data");
                 viewProfileService.getData()
-                    .success(function (response) {
-                        if (response) {
-                            $scope.profile = response;
-                            $scope.tmpNickName = $scope.profile.nickName;
-                            setViewNickName();
-                        }
-                    })
-                    .error(function (error) {
-                        console.log(error);
-                    });
-            
+                        .success(function (response) {
+                            if (response) {
+                                $scope.profile = response;
+                                
+                                setViewNickName();
+                            }
+                        })
+                        .error(function (error) {
+                            console.log(error);
+                        });
+                        console.log("got new data");
             };
-            
-            
-            var setViewNickName = function(){
-                if($scope.tmpNickName === ""){
+
+
+            var setViewNickName = function () {
+                if ($scope.tmpNickName === "") {
                     $scope.nickName = false;
                     console.log($scope.tmpNickName);
                 }
-                else{
+                else {
                     $scope.nickName = true;
-                } 
-                
+                }
+
             };
-            
-            
-            var setEditProfileView = function(){
+
+
+            var setEditProfileView = function () {
                 getCurrentProfile();
-                
             };
+
+            //reset all input fields 
+            var tearDown = function () {
+                $scope.homeTownSuggestions = "";
+                $scope.chosenHomeTown = "";
+                $scope.hometown = "";
+                keyPressCount = 0;
+                
+                setEditProfileView();
+            };
+
             $scope.tmpNickName = "";
-            $scope.chosenHomeTown;
+            $scope.chosenHomeTown = ""; //the data that is used to set/update the home town on server
             $scope.homeTownSuggestions = [];
             $scope.nickname = "";
             $scope.nickName = false;
             var keyPressCount = 0;
-            
+
             setEditProfileView();
+
+
+            
 
         }]);
 }());
--- app/components/user/editProfile/editProfileService.js
+++ app/components/user/editProfile/editProfileService.js
@@ -20,14 +20,48 @@
                     };
 
                     service.setHomeTown = function (hometown) {
-                        return $http.get('http://pioneerroad.com.au:8081/api/v1/user/' + $localStorage.token.id + '/profile/update/hometown',
+                        return $http.put('http://pioneerroad.com.au:8081/api/v1/user/' + $localStorage.token.id + '/profile/update/hometown',
                                 data = {
-                                    homeTown: hometown
+                                    homeTownId: hometown
                                 }
                         );
                     };
-
+                    getLocation();
                     return service;
+
+                    //geolocation stuff
+
+                  //  var x = document.getElementById("location");
+
+                    function getLocation() {
+                        if (navigator.geolocation) {
+                            navigator.geolocation.getCurrentPosition(showError);
+                        } else {
+                           // x.innerHTML = "Geolocation is not supported by this browser.";
+                        }
+                    }
+
+                   /* function showPosition(position) {
+                        x.innerHTML = "Latitude: " + position.coords.latitude +
+                                "<br>Longitude: " + position.coords.longitude;
+                    } */
+
+                    function showError(error) {
+                        switch (error.code) {
+                            case error.PERMISSION_DENIED:
+                             //   x.innerHTML = "User denied the request for Geolocation."
+                                break;
+                            case error.POSITION_UNAVAILABLE:
+                            //    x.innerHTML = "Location information is unavailable."
+                                break;
+                            case error.TIMEOUT:
+                            //    x.innerHTML = "The request to get user location timed out."
+                                break;
+                            case error.UNKNOWN_ERROR:
+                           //     x.innerHTML = "An unknown error occurred."
+                                break;
+                        }
+                    }
                 }
             ]);
 }());
