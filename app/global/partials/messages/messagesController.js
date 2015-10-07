580712c005ac710e7097142661b71ba74d78da9b
matthew <matthew.schwarze@gmail.com>
2015-10-07 12:34:25 +1100

    stuff

--- app/global/partials/messages/messagesController.js
+++ app/global/partials/messages/messagesController.js
@@ -1,11 +1,38 @@
 (function () {
     var app = angular.module("pioneerRoad");
-    
-    app.controller('messagesController', ['$scope', '$rootScope', '$location', '$sce', function ($scope, $rootScope, $location, $sce) {
-            
-            
-                $rootScope.Title = $sce.trustAsHtml("Messages");
-                $rootScope.Link = $sce.trustAsHtml("");
-           
+
+    app.controller('messagesController', ['$scope', '$rootScope', '$location', '$sce', 'messagesService', 'loginRedirect', function ($scope, $rootScope, $location, $sce, messagesService, loginRedirect) {
+
+            if (!loginRedirect.checkLogin()) {
+                $location.path("/login");
+            }
+
+            $rootScope.Title = $sce.trustAsHtml("Messages");
+            $rootScope.Link = $sce.trustAsHtml("");
+
+            var getThreads = function () {
+                messagesService.getThread()
+                        .success(function (response) {
+                            console.log(response);
+                            for (i = 0; i < response.length; i++) { // for each request
+                                thread = response;
+
+                                if (response[i].profilePhoto === null) {
+                                    friend.profilePic = "https://s3-ap-southeast-2.amazonaws.com/images.pioneerroad.com.au/ui-images/user-profile-default-img.svg";
+                                }
+                                else {
+                                    friend.profilePic = "https://s3-ap-southeast-2.amazonaws.com/images.pioneerroad.com.au/user-photos/" + response[i].userAccountId + "/profile-photo/" + response[i].profilePhoto.medium;
+                                }
+
+                                $scope.threads.push(thread);
+                                thread = {};
+                            }
+                        })
+                        .error(function (error) {
+                            console.log(error);
+                            threads = [];
+                        });
+            };
+            getThreads();
         }]);
 }());
--- app/global/partials/messages/messagesService.js
+++ app/global/partials/messages/messagesService.js
@@ -1,9 +1,39 @@
 (function () {
     var app = angular.module("pioneerRoad");
 
-    app.factory('messagesService', [function () {
-            var service;
-            
+    app.factory('messagesService', ['$http', '$localStorage',
+        function ($http, $localStorage) {
+            var service = {};
+
+            service.getThread = function () {
+                return $http.get('http://pioneerroad.com.au:8081/api/v1/message/user/' + $localStorage.token.id + '/active-threads'
+                        );
+            };
+
+            service.createThread = function (recipients, message) {
+                body = {
+                    recipients: recipients,
+                    content: message
+                };
+                return $http.post('http://pioneerroad.com.au:8081/api/v1/message/user/' + $localStorage.token.id + '/new-thread', body
+                        );
+            };
+
+            service.createMessage = function (threadId, message) {
+
+                body = {
+                    content: message
+                };
+                return $http.put('http://pioneerroad.com.au:8081/api/v1/message/user/' + $localStorage.token.id + '/thread/' + threadId + '/new-message'
+                        );
+            };
+
+            service.readThread = function (threadId) {
+                return $http.get('http://pioneerroad.com.au:8081/api/v1/message/user/' + $localStorage.token.id + '/thread/' + threadId + '/read-thread'
+                        );
+            };
+
+
             return service;
         }]);
 }());
